55603d06ecd9bc1ac12e5953ff7fa60f00d7c9fd
Artem LAPOV <artemlapov@me.com>
2022-11-04 21:39:24 +0100

    Done

--- HW3_2/ViewControllers/MainViewController.swift
+++ HW3_2/ViewControllers/MainViewController.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 
+let emojiUrl = "https://emojihub.herokuapp.com/api/random"
+
 class MainViewController: UIViewController {
 
     @IBAction func startRequestButton() {
-
-
-
-
-
+        fetchEmoji()
     }
     
+}
+
+extension MainViewController {
+    func fetchEmoji() {
+        guard let url = URL(string: emojiUrl) else { return }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let emoji = try decoder.decode(Emoji.self, from: data)
+                print(emoji)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+
+    }
 }
 
